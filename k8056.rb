19577d502c5084f83b71e011f55c939a6017917c
Nicholas J Humfrey <njh@aelius.com>
2010-11-11 08:30:29 +0000

    Added the remaining methods to the K8056 ruby class. Added a []= method for turning on and off specific relays.

--- k8056.rb
+++ k8056.rb
@@ -2,36 +2,73 @@
 
 require 'rubygems'
 require 'osc'
-include OSC
 
 class K8056
   def initialize(host='localhost', port='8056', card=1)
-    @client = SimpleClient.new(host, port)
+    @client = OSC::SimpleClient.new(host, port)
     @card = card.to_i
   end
-  
-  
+
+  def []=(relay,value)
+    if value
+      set_relay(relay)
+    else
+      clear_relay(relay)
+    end
+  end
+
+  def set(value)
+    @client.send(
+      OSC::Message.new('/k8056/set', 'ii', @card, value.to_i)
+    )
+  end
+
   def set_relay(relay)
     @client.send(
-      Message.new('/k8056/set_relay', 'ii', @card, relay.to_i)
+      OSC::Message.new('/k8056/set_relay', 'ii', @card, relay.to_i)
     )
   end
 
   def clear_relay(relay)
     @client.send(
-      Message.new('/k8056/clear_relay', 'ii', @card, relay.to_i)
+      OSC::Message.new('/k8056/clear_relay', 'ii', @card, relay.to_i)
     )
   end
 
   def toggle_relay(relay)
     @client.send(
-      Message.new('/k8056/toggle_relay', 'ii', @card, relay.to_i)
+      OSC::Message.new('/k8056/toggle_relay', 'ii', @card, relay.to_i)
     )
   end
+
+  def emergency_stop
+    @client.send(
+      OSC::Message.new('/k8056/emergency_stop')
+    )
+  end
+
+  def display_address
+    @client.send(
+      OSC::Message.new('/k8056/display_address')
+    )
+  end
+
+  def set_address(new_address)
+    @client.send(
+      OSC::Message.new('/k8056/set_address', @card, new_address.to_i)
+    )
+  end
+
+  def display_address
+    @client.send(
+      OSC::Message.new('/k8056/reset_address')
+    )
+  end
+
 end
 
 
 if __FILE__ == $0
   k = K8056.new
-  k.toggle_relay(1)
+  (0..0xff).each {|i| k.set(i); sleep 0.3}
 end
